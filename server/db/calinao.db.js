e76ae3a6f3e62a12f0731ee6b0ca21b11f510d85
Justmyr Gutierrez <91595529+justmyr92@users.noreply.github.com>
2023-12-05 19:50:52 +0800

    Update calinao.db.js

--- server/db/calinao.db.js
+++ server/db/calinao.db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    host: "localhost",
+    host: "54.179.35.184",
     database: "calinao_db",
     password: "dyasmir",
     port: 5432,
